9d6acc8c9566685c9ddbc82efda956ae02781033
juswaa101 <joshuayaacoub33@gmail.com>
2024-06-27 13:43:38 +0800

    first commit

---
+++ resources/js/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/Home.vue";
+import NotFound from "../components/404-Page.vue";
+import Login from "../components/auth/Login.vue";
+import Register from "../components/auth/Register.vue";
+import Dashboard from "../components/Dashboard.vue";
+
+const routes = [
+    {
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        component: Register
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: Dashboard,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: NotFound
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    linkExactActiveClass: 'active',
+    routes,
+});
+
+// Navigation guard to check if the route requires authentication
+router.beforeEach((to, from, next) => {
+    // Check if the route requires authentication and the user is not authenticated
+    if (to.meta.requiresAuth && !localStorage.getItem('token')) {
+        // Redirect to the login page
+        next({ name: 'Login' });
+    } else {
+        // Proceed to the route
+        next();
+    }
+});
+
+export default router;
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\Auth\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/v1/login', [LoginController::class, 'loginAuth'])->name('login.auth');
+Route::post('/v1/register', [RegisterController::class, 'registerAuth'])->name('register.auth');
